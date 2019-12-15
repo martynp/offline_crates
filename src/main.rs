aa8123ee4d7ead2f4456fb0bca4e5700464e4901
martynp <12006448+martynp@users.noreply.github.com>
2019-12-15 14:58:06 +0000

    Refactor of main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-
 use git2::Repository;
 
 use std::fs::File;
-use std::io::{prelude::*};
+use std::io::prelude::*;
 use std::path::Path;
 
 use progress_bar::color::{Color, Style};
@@ -10,10 +9,10 @@ use progress_bar::progress_bar::ProgressBar;
 
 use repository::*;
 
-extern crate reqwest;
 extern crate clap;
+extern crate reqwest;
 
-use clap::{Arg, App};
+use clap::{App, Arg};
 
 const CRATES_URL: &str = "https://github.com/rust-lang/crates.io-index";
 
@@ -44,30 +43,34 @@ fn fast_forward(path: &Path) -> Result<(), git2::Error> {
     }
 }
 
-
 fn main() {
-
     let mut progress_bar = ProgressBar::new(0);
 
     // Using clap to parse command line options
     let matches = App::new("Crates.io Mirror")
         .version("1.0")
         .author("Martyn P")
-        .arg(Arg::with_name("index")
-             .short("i")
-             .long("index")
-             .help("Location for crates.io-index")
-             .takes_value(true))
-        .arg(Arg::with_name("store")
-             .short("s")
-             .long("store")
-             .help("Location for create file store")
-             .takes_value(true))
-        .arg(Arg::with_name("diff")
-             .short("d")
-             .long("diff")
-             .help("Create list of new packages")
-             .takes_value(true))
+        .arg(
+            Arg::with_name("index")
+                .short("i")
+                .long("index")
+                .help("Location for crates.io-index")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("store")
+                .short("s")
+                .long("store")
+                .help("Location for create file store")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("diff")
+                .short("d")
+                .long("diff")
+                .help("Create list of new packages")
+                .takes_value(true),
+        )
         .get_matches();
 
     // Extract the command line arguments
@@ -75,16 +78,14 @@ fn main() {
     let mut store_path = matches.value_of("store").unwrap_or("./crates");
 
     let repo_dir = Path::new(git_path);
-    let store_dir = Path::new(store_path);
 
     // Check to see if the git repository for the crates exists
     if repo_dir.exists() && repo_dir.is_dir() {
-
         progress_bar.print_info(
-            "Info", 
+            "Info",
             &format!("Index directory exists, updating..."),
             Color::Green,
-            Style::Bold
+            Style::Bold,
         );
         println!("");
 
@@ -107,12 +108,11 @@ fn main() {
         }
     // If directory does not exist, then clone it!
     } else {
-
         progress_bar.print_info(
-            "Info", 
+            "Info",
             &format!("Index directory does not exist, cloning..."),
             Color::Green,
-            Style::Bold
+            Style::Bold,
         );
         println!("");
 
@@ -126,44 +126,24 @@ fn main() {
     let mut files = Vec::new();
     repository::walk_repo(&repo_dir, &git_path, &mut files).unwrap();
 
-//    let mut new_file = Vec::new();
-//    for _ in 0..1000 {
-//        new_file.push(files.pop().unwrap());
-//    }
-//    let mut files = new_file;
-
+    // Get metadata from the index repository
     let mut packages: Vec<Package> = Vec::new();
     repository::get_package_info(&mut files, &mut packages, git_path, &mut store_path).unwrap();
 
-    /*
-    let mut progress_bar = ProgressBar::new(packages.len());
-    progress_bar.set_action("Duplicates", Color::Blue, Style::Bold);
-    for package in packages.clone() {
-        for candidate in packages.clone() {
-            if candidate.name == package.name && candidate.version == package.version {
-                if candidate.checksum != package.checksum {
-                    progress_bar.print_info("Failure", &format!("{}-{} - duplicate found", package.name, package.version), Color::Red, Style::Bold);
-                }
-            }
-        }
-        progress_bar.inc();
-    }
-    progress_bar.print_info("Duplicates", "Complete", Color::Green, Style::Bold);
-    println!("");
-    */
-
+    // Verify the store
     let mut missing_files: Vec<Package> = Vec::new();
     repository::verify_store(&mut packages, &mut missing_files).unwrap();
 
+    // If a diff is required, save it
     if matches.is_present("diff") {
         let diff_path = matches.value_of("diff").unwrap();
         let file = File::create(&diff_path).unwrap();
         let mut file = std::io::LineWriter::new(file);
         for package in missing_files.clone() {
-            file.write_all(format!("{}\n", package.relative_path).as_bytes()).unwrap();
+            file.write_all(format!("{}\n", package.relative_path).as_bytes())
+                .unwrap();
         }
     }
 
     repository::download_packages(&mut missing_files, 20).unwrap();
 }
-
